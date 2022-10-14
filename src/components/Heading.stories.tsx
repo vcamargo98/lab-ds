ac18cb99c9d31aca91942ee28bb9ff41fbdbc92f
Vitor <vcamargo96@yahoo.com>
2022-10-13 22:29:30 -0300

    initial commit

---
+++ src/components/Heading.stories.tsx
@@ -0,0 +1,52 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Heading, HeadingProps } from './Heading'
+
+export default {
+    title: 'Components/Heading',
+    component: Heading,
+    args: {
+        children: 'Lorem ipsum.',
+        size:  'md',
+    },
+    argTypes: {
+        size: {
+            options: ['sm','md','lg'],
+            control: {
+                type: 'inline-radio'
+            }
+        }
+    }
+} as Meta<HeadingProps>
+
+export const Default: StoryObj = {}
+export const Small: StoryObj = {
+    args: {
+        size: 'sm',
+    }
+}
+export const Large: StoryObj = {
+    args: {
+        size: 'lg',
+    }
+}
+
+export const CustomComponent: StoryObj<HeadingProps> = {
+    args: {
+        asChild: true,
+        children: (
+            <h1>Heading with h1</h1>
+        )
+    },
+    argTypes: {
+        children: {
+            table: {
+                disable: true,
+            }
+        },
+        asChild: {
+            table: {
+                disable: true,
+            }
+        }
+    }
+}

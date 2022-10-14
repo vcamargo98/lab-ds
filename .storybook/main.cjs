de1f7ff83480485edd72b4e7e1609be13789170d
Vitor <vcamargo96@yahoo.com>
2022-10-13 22:54:14 -0300

    ci: Add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION'){
-      config.base = '/lab-ds'
+      config.base = '/lab-ds/'
     }
     return config
   }
